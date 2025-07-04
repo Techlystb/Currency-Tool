fca9975d8746c6140cfde6dc62c5c87e6de02160
Techlystb blogs <techlystb@gmail.com>
2025-07-04 22:28:21 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
 
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("currency-tool-v1").then(cache => {
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "app.js",
+        "manifest.json",
+        "icon.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(resp => {
+      return resp || fetch(e.request);
+    })
+  );
+});
+
